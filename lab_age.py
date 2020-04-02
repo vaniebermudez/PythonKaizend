cd0ca1130d888765dbaad0695c5874ac9c2deda3
vaniebermudez <vbbermudez1@up.edu.ph>
2020-04-02 22:59:34 +0800

    Kaizend session-3 update

--- lab_age.py
+++
@@ -1,6 +0,0 @@
-name = input("What is your name? ")
-birthdate = input("What is your birthdate? ")
-age = int(input("How old are you? "))
-print(f"{name} was born on {birthdate}")
-print(f"Half of your age is {age / 2}")
-
--- lab_sys_argv.py
+++
@@ -1,4 +0,0 @@
-import sys
-
-print(f"Positional arguments: {sys.argv[1:]}")
-print(f"First argument: {sys.argv[1]}")
