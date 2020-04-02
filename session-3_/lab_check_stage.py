b78e6c9686b46841c961922d0ce0c9708a9241a8
vaniebermudez <vbbermudez1@up.edu.ph>
2020-04-02 23:02:03 +0800

    Kaizend session 3

---
+++ session-3_/lab_check_stage.py
@@ -0,0 +1,12 @@
+import os
+
+#stage = os.environ["STAGE"].upper()
+stage = os.getenv("STAGE", "dev").upper()
+
+output = f"We're running in {stage}"
+
+if stage.startswith("PROD"):
+    output = "DANGER!!! - " + output
+
+print(output)
+
